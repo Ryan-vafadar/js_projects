1384f09e6574eaa02eb0aac1297a428314c50ec3
ryan vafadar <108798153+Ryan-vafadar@users.noreply.github.com>
2023-01-05 15:43:09 +0330

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,51 @@
+alert("hello world");
+prompt("my name is hamid");
+prompt("hi there");
+document.write("<h1>i love world</h1>");
+document.getElementById("hhh").innerHTML="i love my life";
+var date = Date();
+document.write(date+"<br>");
+var status;
+status=5+2;
+document.write(status+"<br>");
+var hiii;
+hiii="student";
+hiii+=" uni<br>vafadar<br>hamid<br>"
+document.write(hello);
+status == 7;
+document.write(status+"<br>");
+status == 12;
+document.write(12>7);
+var x = status;
+function welcomeMsg(){
+    alert("welcome to javascript");
+}
+welcomeMsg();
+function classJava(temp){
+    alert(temp);
+    return temp;
+}
+classJava("java");
+document.write("width"+"<br>");
+var y = 5;
+var x = y--;
+document.write("demo"+"<br>").innerHTML=y;
+document.write("demo2").innerHTML= x;
+var ja = window.location;
+document.write(ja);
+
+function hey(){
+    document.write("salam");
+}
+function tiger(){
+    document.write("tiger is brave");
+}
+
+function tdate(){
+    document.write(date);
+}
+
+function jam(){
+    document.write(8+23);
+}
+
